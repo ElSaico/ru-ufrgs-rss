ad861720af52841ac0e12535407824b6df686315
Bruno Marques <bomarques@inf.ufrgs.br>
2012-04-30 17:12:25 +0000

    URL não quebra mais em fins de semana

--- tools.py
+++ tools.py
@@ -15,23 +15,26 @@ def ru_ufrgs():
 	week_day = date.today().weekday()
 	resource = urllib2.urlopen("http://www.ufrgs.br/ufrgs/ru")
 	page = BeautifulSoup(resource)
+	items = []
 	for day in page.find_all("div", "dia"):
 		day_name = day.h3.contents[0]
 		if day_name == WEEK_DAYS[week_day]:
 			desc = ', '.join([item.strip() for item in day.div.contents if not hasattr(item, 'contents')])
-			feed = RSS2(
-				title=u"Cardápio do RU-UFRGS - diário",
+			items.append(RSSItem(
+				title = '%s (%s)' % (day_name, date.today().strftime('%d/%m/%Y')),
 				link='http://www.ufrgs.br/ufrgs/ru',
-				description=u"Cardápio do dia no Restaurante Universitário da UFRGS",
-				pubDate=datetime.today(),
-				items=[RSSItem(
-					title = '%s (%s)' % (day_name, date.today().strftime('%d/%m/%Y')),
-					link='http://www.ufrgs.br/ufrgs/ru',
-					description=desc,
-					guid=Guid(date.today().isoformat()),
-				)]
-			)
-			return feed.to_xml()
+				description=desc,
+				guid=Guid(date.today().isoformat()),
+			))
+			break
+	feed = RSS2(
+		title=u"Cardápio do RU-UFRGS - diário",
+		link='http://www.ufrgs.br/ufrgs/ru',
+		description=u"Cardápio do dia no Restaurante Universitário da UFRGS",
+		pubDate=datetime.today(),
+		items=items,
+	)
+	return feed.to_xml()
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
